6d49d0e07e5072d61081146cea00825c9c0a2744
Rahul Kumar Pandey <rp201291@gmail.com>
2023-10-13 21:18:09 +0530

    final commit

--- 04-REPORTS-API_Mini-Project_02/src/main/java/com/app/rest/ReportRestController.java
+++ 04-REPORTS-API_Mini-Project_02/src/main/java/com/app/rest/ReportRestController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.app.response.SearchResponse;
 import com.app.service.ReportService;
 
 @RestController
+@CrossOrigin
 public class ReportRestController {
 
 	@Autowired
